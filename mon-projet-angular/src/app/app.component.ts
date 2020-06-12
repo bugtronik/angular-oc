4ed8b2a8cf2616f2ce9f579e6e9e99dec8b747b1
bugtronik <amboumba@setrag.com>
2020-06-12 13:20:45 +0100

    Communication entre component - En cours

--- mon-projet-angular/src/app/app.component.ts
+++ mon-projet-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppareilService } from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	isAuth = false;
 	lastUpdate = new Promise((resolve, reject) => {
 		const date = new Date();
@@ -16,22 +17,10 @@ export class AppComponent {
 		);
 	});
 
-	appareils = [
-		{
-			name: 'Machine à laver',
-			status: 'éteint'
-		},
-		{
-			name: 'Frigo',
-			status: 'allumé'
-		},
-		{
-			name: 'Ordinateur',
-			status: 'éteint'
-		}
-	];
 
-	constructor() {
+	appareils: any[];
+
+	constructor(private appareilService: AppareilService) {
 		setTimeout(
 			() => {
 				this.isAuth = true;
@@ -39,7 +28,19 @@ export class AppComponent {
 		);
 	}
 
+	ngOnInit() {
+		this.appareils = this.appareilService.appareils;
+	}
+
 	onAllumer() {
-		console.log('On allume tout !');
+		this.appareilService.switchOnAll();
+	}
+
+	onEteindre() {
+		if(confirm('Êtes-vous sûr de vouloir éteindre tous vos appareils ?')) {
+			this.appareilService.switchOffAll();
+		} else {
+			return null;
+		}
 	}
 }
